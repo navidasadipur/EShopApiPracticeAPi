bfd2380bd202542f012668e35c321259cc3d3346
1089 <1089@PC434>
2022-01-08 11:06:58 +0330

    add TestProject and CustomerTests of type MSTest

--- EshpApi.Test/CustomerTests.cs
+++ EshpApi.Test/CustomerTests.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 
@@ -23,7 +24,32 @@ namespace EshpApi.Test
         [TestMethod]
         public void CustomerGetAllTest()
         {
-            var request = new HttpRequestMessage
+            var request = new HttpRequestMessage(new HttpMethod("Get"), "/api/customers");
+
+            var resoponse = _client.SendAsync(request).Result;
+
+            Assert.AreEqual(HttpStatusCode.OK, resoponse.StatusCode);
+        }
+
+        [TestMethod]
+        [DataRow(1)]
+        public void CustomerGetOneTest(int id)
+        {
+            var request = new HttpRequestMessage(new HttpMethod("Get"), $"/api/Customers/{id}");
+
+            var respons = _client.SendAsync(request).Result;
+
+            Assert.AreEqual(HttpStatusCode.OK, respons.StatusCode);
+        }
+
+        [TestMethod]
+        public void CustomerPostTest()
+        {
+            var request = new HttpRequestMessage(new HttpMethod("Post"), $"/api/customers");
+
+            var response = _client.SendAsync(request).Result;
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
         }
     }
 }
