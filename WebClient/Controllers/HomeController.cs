1039686352d1cdd28606304d73140ca8328f4969
1089 <1089@PC434>
2022-01-08 14:18:49 +0330

    add webClient project to call and Get data from api

---
+++ WebClient/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebClient.Models;
+
+namespace WebClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private CustomerRepository _customerRepository;
+
+        public HomeController(ILogger<HomeController> logger, CustomerRepository customerRepository)
+        {
+            _logger = logger;
+            _customerRepository = customerRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_customerRepository.GetAllCustomer());
+        }
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebClient/Models/CustomerRepository.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebClient.Models
+{
+    public class CustomerRepository
+    {
+        private string apiUrl = "http://localhost:5247/api/customers";
+        HttpClient _client;
+
+        public CustomerRepository()
+        {
+            _client = new HttpClient();
+        }
+
+        public List<Customer> GetAllCustomer()
+        {
+            var result = _client.GetStringAsync(apiUrl).Result;
+
+            List<Customer> list = JsonConvert.DeserializeObject<List<Customer>>(result);
+
+            return list;
+        }
+
+        public Customer GetCutomerById(int customerId)
+        {
+            var result = _client.GetStringAsync(apiUrl + "/" + customerId).Result;
+
+            Customer customer = JsonConvert.DeserializeObject<Customer>(result);
+
+            return customer;
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            string jsonCustomer = JsonConvert.SerializeObject(customer);
+
+            StringContent content = new StringContent(jsonCustomer, Encoding.UTF8, "application/json");
+
+            var res = _client.PostAsync(apiUrl, content).Result;
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            string jsonCustomer = JsonConvert.SerializeObject(customer);
+
+            StringContent content = new StringContent(jsonCustomer, Encoding.UTF8, "application/json");
+
+            var res = _client.PutAsync(apiUrl, content).Result;
+        }
+
+        public void DeleteCustomer(int customerId)
+        {
+            var res = _client.DeleteAsync(apiUrl + "/" + customerId).Result;
+        }
+    }
+
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
